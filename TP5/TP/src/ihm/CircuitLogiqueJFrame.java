66770fe041f450cdf72f27dac4b0d57c4083de49
bruchong <bruchong@esXXXX.esisar.grenoble-inp.fr>
2016-05-12 08:29:25 +0200

    tp5

---
+++ TP5/TP/src/ihm/CircuitLogiqueJFrame.java
@@ -0,0 +1,227 @@
+package ihm;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import gates.LogicGate;
+import gates.and;
+import gates.button;
+import gates.gate;
+import gates.led;
+import gates.nand;
+import gates.nor;
+import gates.not;
+import gates.or;
+import gates.xor;
+
+/**
+ * Cette classe représente la fenêtre principale de l'interface graphique
+ * 
+ * Elle contient le menu et le conteneur de dessin
+ * 
+ * @author khalfie
+ *
+ */
+public class CircuitLogiqueJFrame extends JFrame {
+
+	/**
+	 * Panneau principal qui contiendra le menu et le panneau de dessin
+	 */
+	private JPanel contentPane;
+
+	/**
+	 * Panneau de dessin
+	 */
+	private LogicGateSimulatorJPanel lgsPanel = new LogicGateSimulatorJPanel();
+
+	/**
+	 * Initialise la fenêtre principale
+	 */
+	public CircuitLogiqueJFrame() {
+
+		setResizable(false);
+		setTitle("Logic Gate Simulator");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(50, 50, 800, 640);
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(5, 0));
+		setContentPane(contentPane);
+
+		createToolBar();
+		contentPane.add(lgsPanel, BorderLayout.CENTER);
+	}
+
+	/**
+	 * Création du toolbar
+	 */
+	private void createToolBar() {
+
+		JToolBar toolbar = new JToolBar();
+
+		ImageIcon iconNew = new ImageIcon("images/new.png");
+		ImageIcon iconLoad = new ImageIcon("images/open.gif");
+		ImageIcon iconSave = new ImageIcon("images/save.png");
+		ImageIcon iconValidate = new ImageIcon("images/validate.png");
+		ImageIcon iconSimulate = new ImageIcon("images/gears.png");
+
+		JButton buttonNew = new JButton(iconNew);
+		JButton buttonLoad = new JButton(iconLoad);
+		JButton buttonSave = new JButton(iconSave);
+		JButton buttonValidate = new JButton(iconValidate);
+		JButton buttonSimulate = new JButton(iconSimulate);
+
+		buttonNew.setToolTipText("Créer un nouveau circuit logique");
+		buttonLoad.setToolTipText("Charger un fichier (*.boole) contenant un circuit logique");
+		buttonSave.setToolTipText("Sauvegarder un circuit logique dans un fichier (*.boole)");
+		buttonValidate.setToolTipText("Vérifier si un circuit logique est fermé");
+		buttonSimulate.setToolTipText("Simuler le résultat d'un circuit logique");
+
+		toolbar.add(buttonNew);
+		toolbar.add(buttonLoad);
+		toolbar.add(buttonSave);
+		toolbar.add(buttonValidate);
+		toolbar.add(buttonSimulate);
+
+		buttonSimulate.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (gate g : LogicOperatorJPanel.lGates) {
+					if (g instanceof gates.led) {
+						if (((led) g).ValiderCircuitFerme()) {
+							boolean simulatedValue = ((led) g).Process();
+							LogicGateSimulatorJPanel logicGateSimulatorJPanel = (LogicGateSimulatorJPanel) contentPane
+									.getComponent(1);
+							JPanel drawingPanel = (JPanel) logicGateSimulatorJPanel.getComponent(1);
+
+							for (Component cp : drawingPanel.getComponents()) {
+								LogicOperatorJPanel operateur = (LogicOperatorJPanel) cp;
+								if (operateur.gate.logicGate == LogicGate.LED) {
+									operateur.SimulateCircuit(simulatedValue);
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+		});
+
+		buttonNew.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				LogicOperatorJPanel.lGates = new ArrayList<gates.gate>();
+				contentPane.remove(lgsPanel);
+				lgsPanel = new LogicGateSimulatorJPanel();
+				contentPane.add(lgsPanel, BorderLayout.CENTER);
+				contentPane.validate();
+				contentPane.repaint();
+			}
+		});
+		buttonValidate.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				for (gate g : LogicOperatorJPanel.lGates) {
+					if (g instanceof gates.led) {
+						if (((led) g).ValiderCircuitFerme()) {
+							JOptionPane.showMessageDialog(null, "Le circcuit est bien fermé",
+									"InfoBox: " + "Circuit Fermé", JOptionPane.INFORMATION_MESSAGE);
+						} else {
+							JOptionPane.showMessageDialog(null,
+									"Le circcuit n'est pas fermé. Merci de connecter toutes les entrées sorties",
+									"InfoBox: " + "Circuit Non Fermé", JOptionPane.WARNING_MESSAGE);
+						}
+						return;
+					}
+				}
+				JOptionPane.showMessageDialog(null, "Un circuit logique doit contenir une seule LED.",
+						"InfoBox: " + "Pas de LED", JOptionPane.ERROR_MESSAGE);
+			}
+		});
+		buttonSave.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (gate g : LogicOperatorJPanel.lGates) {
+					if (g instanceof gates.led) {
+						if (((led) g).ValiderCircuitFerme()) {
+							try {
+								JFileChooser fileChooser = new JFileChooser();
+								fileChooser.setFileFilter(new FileNameExtensionFilter("Circuits logiques", "boole"));
+								if (fileChooser.showSaveDialog(getParent()) == JFileChooser.APPROVE_OPTION) {
+									File file = fileChooser.getSelectedFile();
+									if (!file.getPath().toLowerCase().endsWith(".boole")) {
+
+										file = new File(file.getPath() + ".boole");
+									}
+									JAXBContext jaxbContext = JAXBContext.newInstance(and.class, button.class,
+											nand.class, nor.class, not.class, or.class, xor.class, led.class);
+									Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+									jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+									jaxbMarshaller.marshal(g, file);
+									jaxbMarshaller.marshal(g, System.out);
+								}
+							} catch (JAXBException ex) {
+								ex.printStackTrace();
+							}
+						}
+						return;
+					}
+				}
+			}
+		});
+		buttonLoad.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+				fc.setCurrentDirectory(new File(System.getProperty("user.home")));
+				fc.setDialogTitle("Choisir un Circuit Logique");
+				fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+				fc.setFileFilter(new FileNameExtensionFilter("Circuits logiques", "boole"));
+				if (fc.showOpenDialog(getParent()) == JFileChooser.APPROVE_OPTION) {
+					try {
+						File file = fc.getSelectedFile();
+						JAXBContext jaxbContext = JAXBContext.newInstance(and.class, button.class, nand.class,
+								nor.class, not.class, or.class, xor.class, led.class);
+						Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+						led l = (led) jaxbUnmarshaller.unmarshal(file);
+						contentPane.remove(lgsPanel);
+						lgsPanel = new LogicGateSimulatorJPanel(l);
+						contentPane.add(lgsPanel, BorderLayout.CENTER);
+						contentPane.validate();
+						contentPane.repaint();
+					} catch (JAXBException ex) {
+						ex.printStackTrace();
+					}
+				}
+
+			}
+		});
+		contentPane.add(toolbar, BorderLayout.NORTH);
+	}
+
+	public static void main(String[] args) {
+		CircuitLogiqueJFrame frame = new CircuitLogiqueJFrame();
+		frame.setVisible(true);
+	}
+}
---
+++ TP5/TP/src/ihm/LogicOperatorJPanel.java
@@ -0,0 +1,338 @@
+package ihm;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+
+import gates.LogicGate;
+import gates.gate;
+
+/**
+ * Cette classe représente un opérateur logique dans le panneau de dessin
+ * 
+ * @author khalfie
+ *
+ */
+public class LogicOperatorJPanel extends JPanel {
+
+	/**
+	 * le extrémités d'une connexion en cours de dessin.
+	 */
+	public static int xBegin, yBegin, xEnd, yEnd = 0;
+
+	/**
+	 * le port logique sélectionné depuis la toolbox.
+	 */
+	gates.gate gate;
+
+	/**
+	 * les ports logiques reliés par une connexion en cours de dessin.
+	 */
+	static gates.gate in1Gate, in2Gate, outGate = null;
+
+	/**
+	 * la liste des ports logiques dessinés
+	 */
+	public static List<gates.gate> lGates = new ArrayList<gates.gate>();
+
+	JComponent gateImage = null;
+
+	/**
+	 * Initialise le port logique à dessiner
+	 * 
+	 * @param g
+	 *            port logique correspondant
+	 * @param logicGate
+	 *            le type de port logique à dessiner
+	 * @param x
+	 *            la position de la porte logique sur le conteneur de dessin
+	 * @param y
+	 *            la position de la porte logique sur le conteneur de dessin
+	 */
+	public LogicOperatorJPanel(gate g, LogicGate logicGate, int x, int y) {
+		this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+
+		JPanel connectorsPanel = new JPanel();
+		connectorsPanel.setLayout(new BoxLayout(connectorsPanel, BoxLayout.PAGE_AXIS));
+
+		JLabel connectorIN1 = new JLabel(new ImageIcon("images/connector.png"));
+
+		connectorIN1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+		connectorIN1.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				connectorIN1.setBorder(javax.swing.BorderFactory.createLineBorder(Color.green));
+				in1Gate = gate;
+			}
+
+			public void mouseExited(MouseEvent e) {
+				connectorIN1.setBorder(javax.swing.BorderFactory.createLineBorder(Color.black));
+				in1Gate = null;
+			}
+		});
+
+		JLabel connectorIN2 = new JLabel(new ImageIcon("images/connector.png"));
+
+		connectorIN2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+		connectorIN2.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				connectorIN2.setBorder(javax.swing.BorderFactory.createLineBorder(Color.green));
+				in2Gate = gate;
+			}
+
+			public void mouseExited(MouseEvent e) {
+				connectorIN2.setBorder(javax.swing.BorderFactory.createLineBorder(Color.black));
+				in2Gate = null;
+			}
+		});
+
+		JLabel connectorOUT = new JLabel(new ImageIcon("images/connector.png"));
+		connectorOUT.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+		connectorOUT.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				connectorOUT.setBorder(javax.swing.BorderFactory.createLineBorder(Color.red));
+				outGate = gate;
+			}
+
+			public void mouseExited(MouseEvent e) {
+				connectorOUT.setBorder(javax.swing.BorderFactory.createLineBorder(Color.black));
+				outGate = null;
+			}
+
+			/*************************
+			 * BEGIN : Question 3 Partie 2
+			 ******************************/
+			public void mousePressed(MouseEvent e) {
+				// Le dessin commence d'ici, quelles valeurs donner
+				// à xBegin, xEnd, yBegin, yEnd ?
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				// Que fair après la création d'une connection ?
+				// Qu'est-ce qui change pour la variable gate ?
+				// Que faire si on relâche la souris sans connecter
+				// la sortie qu'on est en train de faire glisser à une entrée ?
+			}
+		});
+
+		connectorOUT.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				// On est en train de glisser une sortie et on cheche
+				// une entrée à connecter
+			}
+		});
+		/*************************
+		 * BEGIN : Question 3 Partie 2
+		 ******************************/
+		switch (logicGate) {
+		case AND:
+			if (g == null) {
+				gate = new gates.and();
+			} else {
+				gate = g;
+			}
+
+			gateImage = new JLabel(new ImageIcon("images/AND64.png"));
+
+			connectorsPanel.add(connectorIN1);
+			connectorsPanel.add(Box.createRigidArea(new Dimension(0, 6)));
+			connectorsPanel.add(connectorIN2);
+			this.add(connectorsPanel);
+			this.add(gateImage);
+			this.add(connectorOUT);
+
+			break;
+		case NAND:
+			if (g == null) {
+				gate = new gates.nand();
+			} else {
+				gate = g;
+			}
+			gateImage = new JLabel(new ImageIcon("images/NAND64.png"));
+
+			connectorsPanel.add(connectorIN1);
+			connectorsPanel.add(Box.createRigidArea(new Dimension(0, 6)));
+			connectorsPanel.add(connectorIN2);
+			this.add(connectorsPanel);
+			this.add(gateImage);
+			this.add(connectorOUT);
+
+			break;
+		case NOR:
+			if (g == null) {
+				gate = new gates.nor();
+			} else {
+				gate = g;
+			}
+			gateImage = new JLabel(new ImageIcon("images/NOR64.png"));
+
+			connectorsPanel.add(connectorIN1);
+			connectorsPanel.add(Box.createRigidArea(new Dimension(0, 6)));
+			connectorsPanel.add(connectorIN2);
+			this.add(connectorsPanel);
+			this.add(gateImage);
+			this.add(connectorOUT);
+
+			break;
+		case NOT:
+			if (g == null) {
+				gate = new gates.not();
+			} else {
+				gate = g;
+			}
+			gateImage = new JLabel(new ImageIcon("images/NOT64.png"));
+
+			// On n'ajouter pas lec connecteur d'entrée à un bouton, il n'a que
+			// le connecteur de sortie.
+			// connectorsPanel.add(connectorIN1);
+			// connectorsPanel.add(Box.createRigidArea(new Dimension(0, 6)));
+			// connectorsPanel.add(connectorIN2);
+			connectorsPanel.add(connectorIN1);
+			this.add(connectorsPanel);
+			this.add(gateImage);
+			this.add(connectorOUT);
+
+			break;
+		case OR:
+			if (g == null) {
+				gate = new gates.or();
+			} else {
+				gate = g;
+			}
+			gateImage = new JLabel(new ImageIcon("images/OR64.png"));
+
+			connectorsPanel.add(connectorIN1);
+			connectorsPanel.add(Box.createRigidArea(new Dimension(0, 6)));
+			connectorsPanel.add(connectorIN2);
+			this.add(connectorsPanel);
+			this.add(gateImage);
+			this.add(connectorOUT);
+
+			break;
+		case XOR:
+			if (g == null) {
+				gate = new gates.xor();
+			} else {
+				gate = g;
+			}
+			gateImage = new JLabel(new ImageIcon("images/XOR64.png"));
+
+			connectorsPanel.add(connectorIN1);
+			connectorsPanel.add(Box.createRigidArea(new Dimension(0, 6)));
+			connectorsPanel.add(connectorIN2);
+			this.add(connectorsPanel);
+			this.add(gateImage);
+			this.add(connectorOUT);
+
+			break;
+		case BUTTON:
+			if (g == null) {
+				gate = new gates.button();
+			} else {
+				gate = g;
+			}
+			gateImage = new JToggleButton(new ImageIcon("images/Switch.gif"), false);
+
+			((JToggleButton) gateImage).setContentAreaFilled(false);
+			((JToggleButton) gateImage).setBorderPainted(false);
+			((JToggleButton) gateImage).setFocusPainted(false);
+			((JToggleButton) gateImage).setSelectedIcon(new ImageIcon("images/Switch_on.gif"));
+
+			((JToggleButton) gateImage).setSelected(((gates.button) gate).isValue());
+
+			((JToggleButton) gateImage).addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					JToggleButton btn = (JToggleButton) e.getSource();
+					if (btn.isSelected()) {
+						((gates.button) gate).setValue(true);
+					} else {
+						((gates.button) gate).setValue(false);
+					}
+				}
+			});
+
+			// On n'ajouter pas lec connecteur d'entrée à un bouton, il n'a que
+			// le connecteur de sortie.
+			// connectorsPanel.add(connectorIN1);
+			// connectorsPanel.add(Box.createRigidArea(new Dimension(0, 6)));
+			// connectorsPanel.add(connectorIN2);
+			this.add(gateImage);
+			this.add(connectorOUT);
+			break;
+		case LED:
+			if (g == null) {
+				gate = new gates.led();
+			} else {
+				gate = g;
+			}
+			gateImage = new JToggleButton(new ImageIcon("images/LED.gif"), false);
+
+			((JToggleButton) gateImage).setContentAreaFilled(false);
+			((JToggleButton) gateImage).setBorderPainted(false);
+			((JToggleButton) gateImage).setFocusPainted(false);
+			((JToggleButton) gateImage).setSelectedIcon(new ImageIcon("images/LED_on.gif"));
+
+			((JToggleButton) gateImage).setDisabledSelectedIcon(new ImageIcon("images/LED_on.gif"));
+			((JToggleButton) gateImage).setDisabledIcon(new ImageIcon("images/LED.gif"));
+			((JToggleButton) gateImage).setEnabled(false);
+
+			connectorsPanel.add(connectorIN1);
+			this.add(connectorsPanel);
+			this.add(gateImage);
+			break;
+		case NONE:
+			break;
+		}
+		gate.setX(x);
+		gate.setY(y);
+
+		if (g == null) {
+			lGates.add(gate);
+		}
+
+		/*************************
+		 * BEGIN : Question 2 Partie 2
+		 ******************************/
+		this.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+
+			}
+		});
+
+		this.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+
+			}
+		});
+		/*************************
+		 * END : Question 2 Partie 2
+		 ****************************/
+
+		this.setBounds(x, y, this.getPreferredSize().width, this.getPreferredSize().height);
+
+		this.validate();
+		this.repaint();
+	}
+
+	public void SimulateCircuit(boolean simulatedValue) {
+		((JToggleButton) gateImage).setSelected(simulatedValue);
+	}
+}
